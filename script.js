b40fac668aa9fd7ee771d21d28161c85225ff4f8
Moniqueemh <mohernandez9992@yahoo.com>
2020-09-16 22:39:26 -0700

    updated js, local storage

---
+++ script.js
@@ -0,0 +1,62 @@
+// possible variables
+var hourlyArray;
+var currentTime = moment();
+var currentHour;
+var textBlock = $(".col-8");
+var plannerTask = $("textarea");
+
+
+// getting texts and turning into objects
+if (localStorage.getItem("localHourlyTasks")) {
+    hourlyArray = JSON.parse(localStorage.getItem("localHourlyTasks"));
+} else {
+    hourlyArray = [];
+};
+// current date 
+$("#currentDay").text(`${currentTime.format('dddd, MMMM Do')}`);
+
+// update text block
+function updateCurrentScheduleTime() {
+    textBlock.removeClass('past present future');
+    $.each(textBlock, function (scheduleBlockHour) {
+        if (scheduleBlockHour < (currentTime.hour() - 9)) {
+            $(this).addClass('past');
+        } else if (scheduleBlockHour == (currentTime.hour() - 9)) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
+    });
+    currentHour = currentTime.hour();
+};
+// updating local storage evertime you hit save
+function updateLocalStorage() {
+    event.preventDefault();
+    let btnIndex = Number($(this).attr('id'));
+    $('.alert-success').removeClass('alert-animation');
+    if (plannerTask[btnIndex].value.trim() != "") {
+        hourlyArray[btnIndex] = {
+            time: $(".hour")[btnIndex].textContent.trim(),
+            task: plannerTask[btnIndex].value
+        };
+        localStorage.setItem("localHourlyTasks", JSON.stringify(hourlyArray));
+        setTimeout(function () {
+            $('.alert-success').addClass('alert-animation');
+            $('.alert-success').text(`Successfully saved task at ${$(".hour")[btnIndex].textContent.trim()}!`);
+        }, 100);
+    };
+};
+// after refresh, tasks are saved
+function writeCurrentTasks() {
+    $.each(hourlyArray, function (i) {
+        if (hourlyArray[i]) {
+            plannerTask[i].value = hourlyArray[i].task;
+        };
+    });
+};
+
+
+// Callbacks and Click listener
+updateCurrentScheduleTime();
+writeCurrentTasks();
+$("button").click(updateLocalStorage);
